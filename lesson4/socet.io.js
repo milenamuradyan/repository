83d4b9866d7818a9d5cbbe00f0fd4993c32be37d
Your Name <you@example.com>
2019-05-12 17:03:12 +0400

    lesson 4

---
+++ lesson4/socet.io.js
@@ -0,0 +1,11 @@
+io.on('connection', function (socket) {
+    for(var i in messages) {
+      io.sockets.emit("display message", messages[i]);
+    }
+    socket.on("send message", function (data) {
+        messages.push(data);
+        io.sockets.emit("display message", data);
+    });
+ });
+ 
+ 
